78cefacc8dbc916016595ccd97608415eaf7327d
Alex Billingsley <abillingsley@dyknow.com>
2015-10-19 11:18:47 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 choco install packages.config --acceptlicense --yes
 webpicmd /Install /Products:IIS7
+dism /Online /Enable-Feature /All /FeatureName:IIS-WebSockets /FeatureName:IIS-ASPNET  /FeatureName:IIS-ASPNET45
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g grunt-cli
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g karma-cli
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g bower
