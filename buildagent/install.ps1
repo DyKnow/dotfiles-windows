ee39c1dc3de187ebfbdc0e1e89eeffa8267813ee
Alex Billingsley <abillingsley@dyknow.com>
2016-03-29 11:36:45 -0400

    install npm dependencies on build agent

--- buildagent/install.ps1
+++ buildagent/install.ps1
@@ -1,4 +1,7 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install packages.config --acceptlicense --yes
+& 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g grunt-cli
+& 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g karma-cli
+& 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g bower
 git config --global merge.tool p4merge
 git config --global mergetool.keepBackup false
