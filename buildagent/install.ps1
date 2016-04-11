55981b2ee6956816443773486ca52dcae15ea99d
Alex Billingsley <abillingsley@dyknow.com>
2016-04-11 14:42:35 -0400

    Update install.ps1

--- buildagent/install.ps1
+++ buildagent/install.ps1
@@ -3,6 +3,5 @@ choco install packages.config --acceptlicense --yes
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g grunt-cli
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g karma-cli
 & 'C:\Program Files\nodejs\node.exe' 'C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js' install -g bower
-c:\opscode\chef\embedded\bin\gem install curb
 git config --global merge.tool p4merge
 git config --global mergetool.keepBackup false
